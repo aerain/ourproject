5c95d4566199cf7a48baa6e46e79466c4ee6797d
rbrbrb7290 <ohg429@gmail.com>
2017-06-07 16:46:27 +0900

    20170607

---
+++ Assets/kitchen props/LowPoly Tiles Set/Scripts/movem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movem : MonoBehaviour {
+
+//	private float Dist = 20f;
+
+	void Update () {
+		Vector3 temp = Input.mousePosition;
+		temp.z = 20f;
+		this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+	}
+}
---
+++ Assets/kitchen props/LowPoly Tiles Set/Scripts/sceneManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour 
+{
+	public void LoadScene(string sceneName)
+	{
+		
+		SceneManager.LoadScene(sceneName);
+	}
+}
