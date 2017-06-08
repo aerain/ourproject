77e0fa84b9a04d2513a8d570cd358e8e1ed815c5
Aerain <j40f893@gmail.com>
2017-06-08 19:11:42 +0900

    Door Script test

---
+++ Assets/DoorAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAction : MonoBehaviour {
+    bool up;
+
+    public void Play()
+    {
+        up = true;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (up)
+        {
+            transform.Translate(new Vector3(0, 1, 0));
+        }
+	}
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float MoveSpeed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(0, 0, v) * MoveSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, h, 0));
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (az) az.Run();
+        }
+    }
+        // import 필요없는 객체 선언
+
+    public ActionZone az;
+
+    void OnTriggerEnter(Collider _col)
+    {
+        if (_col.gameObject.tag == "ActionZone")
+        {
+            az = _col.gameObject.GetComponent<ActionZone>();
+        }
+    }
+
+    void OnTriggerExit(Collider _col)
+    {
+        if (_col.gameObject.tag == "ActionZone")
+        {
+            az = null;
+        }
+    }
+    
+}
+
